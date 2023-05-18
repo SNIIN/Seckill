321284bdb6db75423b1443643eb54948ce1c07e3
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:42:33 +0800

    Update startGoods.sh

--- sh/startGoods.sh
+++ sh/startGoods.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-docker service remove goods
+docker service remove seckill-goods
 docker rmi goods
 cd ..
 cd goods
 docker node update --label-add $2=goods $1
 mvn clean package -DskipTests -Dmaven.repo.local=/Seckill
 docker build --build-arg JAR_FILE=goods-0.0.1-SNAPSHOT.jar -t goods .
-docker service create --name goods -p 8083:8083 --constraint node.labels.$2==goods --network mynet goods
+docker service create --name seckill-goods -p 8083:8083 --constraint node.labels.$2==goods --network mynet goods

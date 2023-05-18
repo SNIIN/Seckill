59a6ccfe8667feba59238c6a19db520f246098c1
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:43:05 +0800

    Update startOrder.sh

--- sh/startOrder.sh
+++ sh/startOrder.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-docker service remove  order
+docker service remove  seckill-order
 docker rmi order
 cd ..
 cd order
 docker node update --label-add $2=order $1
 mvn clean package -DskipTests -Dmaven.repo.local=/Seckill
 docker build --build-arg JAR_FILE=order-0.0.1-SNAPSHOT.jar -t order .
-docker service create -d --name order -p 8085:8085 --constraint node.labels.$2==order --network mynet order
+docker service create -d --name seckill-order -p 8085:8085 --constraint node.labels.$2==order --network mynet order

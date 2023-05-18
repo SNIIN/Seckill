38819c62cdf87e32d9366ae44e6b40f42821a222
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:43:20 +0800

    Update startSeckill.sh

--- sh/startSeckill.sh
+++ sh/startSeckill.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-docker service remove seckill
+docker service remove seckill-seckill
 docker rmi seckill
 cd ..
 cd seckill
 docker node update --label-add $2=seckill $1
 mvn clean package -DskipTests -Dmaven.repo.local=/Seckill
 docker build --build-arg JAR_FILE=seckill-0.0.1-SNAPSHOT.jar -t seckill .
-docker service create -d --name seckill -p 8084:8084 --constraint node.labels.$2==seckill --network mynet seckill
+docker service create -d --name seckill-seckill -p 8084:8084 --constraint node.labels.$2==seckill --network mynet seckill

8536e6035b56cfe7e3dc4d1397b33032ab4aa319
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:51:45 +0800

    Update startNacos.sh

--- sh/startNacos.sh
+++ sh/startNacos.sh
@@ -3,4 +3,4 @@ docker service rm nacos
 docker rmi nacos/nacos-server:2.0.3
 docker node update --label-add $2=nacos $1
 docker pull nacos/nacos-server:2.0.3
-docker service create -d --name nacos --constraint node.labels.$2==nacos -p 8848:8848 --network mynet -e MODE=standalone nacos/nacos-server:2.0.3 
+docker service create -d --name nacos --constraint node.labels.$2==nacos -p 8848:8848 --network mynet -e MODE=standalone -e PREFER_HOST_MODE=hostname nacos/nacos-server:2.0.3 

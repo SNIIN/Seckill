320269d0562fcca2cedfa7b1730353073041aa94
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:42:12 +0800

    Update startGateway.sh

--- sh/startGateway.sh
+++ sh/startGateway.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker service remove gateway
+docker service remove seckill-gateway
 docker rmi gateway
 cd ..
 cd gateway
 docker node update --label-add $2=gateway $1
 mvn clean package -DskipTests -Dmaven.repo.local=/Seckill
 docker build --build-arg JAR_FILE=gateway-0.0.1-SNAPSHOT.jar -t gateway .
-docker service create -d --name gateway -p 8090:8090 --constraint node.labels.$2==gateway --network mynet gateway
+docker service create -d --name seckill-gateway -p 8090:8090 --constraint node.labels.$2==gateway --network mynet gateway

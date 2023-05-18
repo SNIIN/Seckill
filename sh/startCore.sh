e0919559d2faa344fb769dba6e4538a1126de9ae
SNIIN <92317070+SNIIN@users.noreply.github.com>
2023-05-18 08:42:49 +0800

    Update startCore.sh

--- sh/startCore.sh
+++ sh/startCore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ..
-docker service remove core
+docker service remove seckill-core
 docker rmi core
 mvn clean install -DskipTests -Dmaven.repo.local=/Seckill
 docker node update --label-add $2=core $1
 cd core
 docker build --build-arg JAR_FILE=core-0.0.1-SNAPSHOT-exec.jar -t core .
-docker service create -d --name core -p 8080:8080 --constraint node.labels.$2==core --network mynet core
+docker service create -d --name seckill-core -p 8080:8080 --constraint node.labels.$2==core --network mynet core
